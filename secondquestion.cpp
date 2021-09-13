d627ee81a57526aa3fb23624a638c6f8c67c6354
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-09-13 22:29:40 +0530

    question2

--- secondquestion.cpp
+++ secondquestion.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   
+   int t;
+   cin>>t;
+   int count=0;
+   while(t--)
+   {
+     int P,V,T;
+     cin>>P>>V>>T;
+      if(P+V+T>=2)
+         count++;
+   }
+  cout<<count<<endl;     
   return 0;
 }
