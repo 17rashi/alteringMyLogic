60ba85f00fe98cc359ce413dc04e349a5f41ea9f
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-09-15 00:08:17 +0530

    day 3

--- forthquestion.cpp
+++ forthquestion.cpp
@@ -6,12 +6,10 @@ int main(){
    cin>>s1;
    int count=1;
    sort(s1.begin(),s1.end());
-   cout<<s1<<endl;
    for(int i=1;i<s1.length();i++)
    {
       if(s1[i]!=s1[i-1])
          count++;
-      cout<<count<<endl;
    }
    if(count%2==0)
       cout<<"CHAT WITH HER!"<<endl;
