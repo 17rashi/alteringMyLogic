50844a3a94d1f3df3d37f76734e226ea226285c2
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-09-14 23:58:34 +0530

    updated

--- forthquestion.cpp
+++ forthquestion.cpp
@@ -2,30 +2,20 @@
 #include<algorithm>
 using namespace std;
 int main(){
-   string s1,s2;
-   cin>>s1>>s2;
-   transform(s1.begin(),s1.end(),s1.begin(),::tolower);
-   transform(s2.begin(),s2.end(),s2.begin(),::tolower);
-   int sum1=0,sum2=0;
-   for(int i='a'||'A';i<s1.length();i++)
-   {
-      sum1+=s1[i];
-   }
-   for(int i='a'||'A';i<s2.length();i++)
-   {
-      sum2+=s2[i];
-   }
-   cout<<sum1<<endl<<sum2<<endl;
+   string s1;
+   cin>>s1;
+   int count=1;
    sort(s1.begin(),s1.end());
-   sort(s2.begin(),s2.end());
-   cout<<endl<<s1<<endl<<s2<<endl;
-   if(s1!=s2 && (sum1==sum2))
-      cout<<1<<endl;
-   else if(sum1<sum2)
-      cout<<"-1"<<endl;
-   else if(sum1>sum2)
-      cout<<"1"<<endl;
-   else if(sum1==sum2)
-      cout<<"0"<<endl;
+   cout<<s1<<endl;
+   for(int i=1;i<s1.length();i++)
+   {
+      if(s1[i]!=s1[i-1])
+         count++;
+      cout<<count<<endl;
+   }
+   if(count%2==0)
+      cout<<"CHAT WITH HER!"<<endl;
+   else
+      cout<<"IGNORE HIM!"<<endl;
   return 0;
 }
