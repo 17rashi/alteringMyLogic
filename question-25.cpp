8b30c4c4729eb4fb54163cfbf509366655953d80
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-09-24 23:40:45 +0530

    Last Updated on 24-09-2021 at 11:40pm

---
+++ question-25.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool areEqual(int arr[],int b[],int n)
+{
+   for(int i=0;i<n;i++)
+   {
+      if(n%2==0)
+      {
+         return false;
+      }
+      if(arr[i]!=b[i])
+         return false;
+      else if(arr[i]==b[i] && arr[0]==1)
+      {
+         if(arr[i+1]-arr[i]==1) 
+            return true;
+      }
+   }
+   return false;
+}
+void reverse(int arr[], int n) {
+    reverse(arr, arr + n);
+}
+int main(){
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+      int arr[n], b[n];
+      for(int i=0;i<n;i++)
+      {
+         cin>>arr[i];
+      }
+      for(int i=0;i<n;i++)
+      {
+         b[i]=arr[i];
+      }
+      reverse(arr,n);
+      if(areEqual(arr,b,n))
+         cout<<"yes"<<endl;
+      else
+         cout<<"no"<<endl;
+   }
+  return 0;
+}
+
