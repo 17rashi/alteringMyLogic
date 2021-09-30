a81655afbbed6b6f2e989ed5992f6751a6243a63
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-09-30 22:21:31 +0530

    Last Updated on 30-09-2021 at 10:21

---
+++ question-32.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int R,B,sum=0;
+   cin>>R>>B;
+   cout<<min(R,B)<<" ";
+   if(R>B)
+      sum=R-B;
+   else
+      sum=B-R;  
+   int ans=sum/2;
+   cout<<ans<<endl;
+  return 0;
+}
