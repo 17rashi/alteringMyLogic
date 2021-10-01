c50726f0c019f044ad55ed9e1a07d27676954b73
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-10-01 22:57:09 +0530

    Last Updated on 01-10-2021 at 10:57 PM

---
+++ question-33.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   long long int t,sum=0;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+      sum=(n-1)/2;
+      cout<<sum<<endl;
+   }
+  return 0;
+}
