bc283ee830c11a1616a4474c15dff6fa7482aeac
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-10-11 10:17:09 +0530

    Last Updated on 11-10-2021 at 10:17 AM

---
+++ question-45.cpp
@@ -0,0 +1,40 @@
+//https://www.codechef.com/CUPA2101/problems/CHEFALT
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+      string str;
+      cin>>str;
+      int count=0, len=str.length();
+      for(int i=0;i<str.length();i++)
+      {
+         if(str[i]=='(')
+         {
+            count++;
+         }
+      }
+      for(int i=0;i<str.length();i++)
+      {
+         if(str[i]==')')
+         {
+            count--;
+         }
+      }
+      if(count>0)
+      {
+         cout<<len-count<<endl;
+      }
+      else
+      {
+         cout<<len+count<<endl;
+      }
+      
+   }  
+  return 0;
+}
