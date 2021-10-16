c3663f1f5cbc9fa7592e5cdb4d2c2450ac86b035
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-10-17 01:31:21 +0530

    Last Updated on 17-10-2021 at 01:31 AM

---
+++ question-51.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/SNCKQL21/problems/LUCKYNUM
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int T;
+   cin>>T;
+   while(T--)
+   {
+      int A,B,C;
+      cin>>A>>B>>C;
+      if(A==7 || B==7 || C==7)
+      {
+         cout<<"YES"<<endl;
+      }
+      else
+      {
+         cout<<"NO"<<endl;
+      }
+   }
+  return 0;
+}
