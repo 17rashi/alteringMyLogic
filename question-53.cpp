bbf3392a15c39afa4dced68db7fa92a83e6ab8cb
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-10-19 00:39:42 +0530

    Last Updated on 19-10-2021 at 12:39 AM

--- question-53.cpp
+++ question-53.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   cout<<" ";
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+      int arr[n];
+      for(int i=0;i<n;i++)
+      {
+         cin>>arr[i];
+      }
+      for(int i=0;i<n;i++)
+      {
+         if(i%2==0)
+         {
+            cout<<arr[i]<<" ";
+         }
+      }
+      cout<<endl;
+   }
   return 0;
 }
