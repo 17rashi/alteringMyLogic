9735754902beb8e9071f398a2f97e81827dc5758
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-11-01 22:30:44 +0530

    Last Updated on 01-11-2021 at 10:30 PM

--- question-61.cpp
+++ question-61.cpp
@@ -1,6 +1,26 @@
+//https://www.codechef.com/problems/CHN15A
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int N,K,count=0;
+      cin>>N>>K;
+      int arr[N];
+      for(int i=0;i<N;i++)
+      {
+         cin>>arr[i];
+      }
+      for(int i=0;i<N;i++)
+      {
+         if((arr[i]+K)%7==0)
+         {
+            count++;
+         }
+      }
+      cout<<count<<endl;
+   }
   return 0;
 }
