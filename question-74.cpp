eb93fecb81b08c63a21eca55cf21f8e36ea44c04
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-11-18 00:21:49 +0530

    Last Updated on 18-11-2021 at 12:21 PM

--- question-74.cpp
+++ question-74.cpp
@@ -1,6 +1,45 @@
+//https://www.codechef.com/problems/RECIPE
+
 #include<bits/stdc++.h>
 using namespace std;
+int gcd(int a,int b)
+{
+   if(a==0)
+   {
+      return b;
+   }
+   return gcd(b%a,a);
+}
+int findGCD(int arr[],int n)
+{
+   int result=arr[0];
+   for(int i=1;i<n;i++)
+   {
+      result=gcd(arr[i],result);
+      if(result==1)
+      {
+         return 1;
+      }
+   }
+   return result;
+}
 int main(){
-   
+   int T;
+   cin>>T;
+   while(T--)
+   {
+      int N;
+      cin>>N;
+      int arr[N];
+      for(int i=0;i<N;i++)
+      {
+         cin>>arr[i];
+      }
+      int m=findGCD(arr,N);
+      for(int i=0;i<N;i++)
+      {
+         cout<<arr[i]/m<<" ";
+      }
+   }
   return 0;
 }
