83c92a56d64eda306d1946a9c622660fc4d85e8b
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-11-22 23:18:37 +0530

    Last Updated on 22-11-2021 at 11:18 PM

---
+++ question-79.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int N,sum=0;
+      cin>>N;
+      int arr[N];
+      for(int i=0;i<N;i++)
+      {
+         cin>>arr[i];
+      }
+      for(int i=0;i<N;i++)
+      {
+         
+      }
+   }
+  return 0;
+}
