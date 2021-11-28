c7e3535c9b13647151db3c6b0629d208f2b7042b
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-11-28 23:20:41 +0530

    Last Updated on 28-11-2021 at 11:20 PM

--- question-81.cpp
+++ question-81.cpp
@@ -5,20 +5,6 @@ int main(){
    cin>>t;
    while(t--)
    {
-      string str;
-      cin>>str;
-      for(int i=0;i<str.length();i++){
-         for(int j=i+1;j<str.length();j++)
-         {
-            if(str[i]==str[j])
-            {
-               cout<<"NO"<<endl;
-               break;
-            }
-         }
-         cout<<"YES"<<endl;
-         break;
-      }
       
    }
   return 0;
