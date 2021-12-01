f193ce23a7aab99677822ee0e6e7ce2328700956
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-12-01 22:55:44 +0530

    Last Updated on 01-12-2021 at 10:55 PM

--- question-81.cpp
+++ question-81.cpp
@@ -1,6 +1,20 @@
+//https://www.codechef.com/problems/FLOW017
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+     int n=3;
+     int arr[3];
+     for(int i=0;i<n;i++)
+     {
+       cin>>arr[i];
+     }
+     sort(arr,arr+n);
+     cout<<arr[1]<<endl;
+   }
   return 0;
 }
