3877d5a8430052c6e23de2ff25db07d3bafbc58f
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-12-03 23:21:24 +0530

    Last Updated on 03-12-2021 at 11:21 PM

--- question-82.cpp
+++ question-82.cpp
@@ -1,6 +1,45 @@
+//https://www.codechef.com/problems/LCH15JAB
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    string s;
+    cin>>s;
+    int max=0;
+    for(int i=0;i<s.length();i++)
+    {
+      int count=0;
+      for(int j=0;j<s.length();j++)
+      {
+        if(s[i]==s[j])
+        {
+          count++;
+        }
+      }
+      if(count>max)
+      {
+        max=count;
+      }
+    }
+      if(s.length()%2==0)
+      {
+        if(max==s.length()-max)
+        {
+          cout<<"YES"<<endl;
+        }
+        else
+        {
+          cout<<"NO"<<endl;
+        } 
+      }
+      else
+      {
+        cout<<"NO"<<endl;
+      } 
+  }
   return 0;
 }
