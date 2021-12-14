f2c191c5d5fbd1f3eeb9795f53cce5c782e6cded
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-12-14 22:58:52 +0530

    Last Updated on 14-12-2021 at 10:58 PM

--- Recursion/checkPalindrome2.cpp
+++ Recursion/checkPalindrome2.cpp
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-void reverse(string str)
-{
-   if(str.length()==0){
-      return;
+bool ispalindrome(string str,int start,int end){
+   if(start>=end){
+      return true;
    }
-   string ros=str.substr(1);
-   reverse(ros);
-   cout<<str[0];
+   return (str[start]==str[end]&&ispalindrome(str,start+1,end-1));
 }
 int main(){
    string str;
    cin>>str;
-   reverse(str);
+   int start=0;
+   int end=str.length()-1;
+   cout<<ispalindrome(str,start,end);
   return 0;
 }
---
+++ Recursion/sumofsequence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sumofseq(int x,int m,int n){
+   if(m==0)
+   {
+      return 1;
+   }
+   int res=pow(x,m)+sumofseq(x,m-1,n);
+   return res%n;
+}
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int x,n,m;
+    cin>>x>>m>>n;
+    cout<<sumofseq(x,m,n);
+  }
+  return 0;
+}
