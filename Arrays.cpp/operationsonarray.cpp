f6f8b1fab46b5fcdef6251eefed9018d6f8d39f7
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-12-27 22:52:12 +0530

    Last Updated on 27-12-2021mat 10:52 PM

--- Arrays.cpp/operationsonarray.cpp
+++ Arrays.cpp/operationsonarray.cpp
@@ -1,4 +1,5 @@
-/*Insertion in an array*/
+/*Insertion in an array
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -14,6 +15,30 @@ int main(){
       if(i==index){
          arr[i]=num;
          cout<<arr[i]<<" ";
+      }
+      else{
+         cout<<arr[i]<<" ";
+      }
+   }
+  return 0;
+}
+*/
+/*delete Operation*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+   int arr[100];
+   for(int i=0;i<n;i++){
+      cin>>arr[i];
+   }
+   int index;
+   cin>>index;
+   for(int i=0;i<n;i++){
+      if(i==index){
+         continue;
       }
       else{
          cout<<arr[i]<<" ";
