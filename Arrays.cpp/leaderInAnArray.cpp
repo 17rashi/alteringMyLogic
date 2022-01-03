e796c4d41aea12b5913398f7861ed0299ccf71a2
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-01-03 14:07:06 +0530

    Optimised solution of leaders in an array

--- Arrays.cpp/leaderInAnArray.cpp
+++ Arrays.cpp/leaderInAnArray.cpp
@@ -1,5 +1,5 @@
 //count leaders in an array - if all the elements on right side is smaller then an element is a leader
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 void leaderInAnArray(int arr[],int size){
@@ -7,7 +7,7 @@ void leaderInAnArray(int arr[],int size){
    for(int i=0;i<size;i++){
       count=0;
       for(int j=i+1;j<size;j++){
-         if(arr[i]<arr[j]){
+         if(arr[i]<=arr[j]){
             count++;
             break;
          }
@@ -20,6 +20,28 @@ void leaderInAnArray(int arr[],int size){
 }
 int main(){
    int arr[]={30,20,10};
+   int size=sizeof(arr)/sizeof(arr[0]);
+   leaderInAnArray(arr,size);
+  return 0;
+}
+*/
+
+//OPTIMISED SOLUTION 
+
+#include<bits/stdc++.h>
+using namespace std;
+void leaderInAnArray(int arr[],int size){
+   int current_Leader=arr[size-1];
+   cout<<current_Leader<<" ";
+   for(int i=size-1;i>=0;i--){
+      if(arr[i]>current_Leader){
+         cout<<arr[i]<<" ";
+         current_Leader=arr[i];
+      }
+   }
+}
+int main(){
+   int arr[]={7,11,4,10,6,5,2};
    int size=sizeof(arr)/sizeof(arr[0]);
    leaderInAnArray(arr,size);
   return 0;
