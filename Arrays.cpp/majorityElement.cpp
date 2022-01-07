981445ec8efe4cb70783dee770c44fd55080bad8
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-01-08 01:41:37 +0530

    optimized solution of majority element

--- Arrays.cpp/majorityElement.cpp
+++ Arrays.cpp/majorityElement.cpp
@@ -1,5 +1,5 @@
 //NAIVE SOLUTION OF majority element in an array.
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int majority(int arr[],int size){
@@ -18,6 +18,43 @@ int majority(int arr[],int size){
 }
 int main(){
    int arr[]={8,3,4,8,8};
+   int size=sizeof(arr)/sizeof(arr[0]);
+   cout<<majority(arr,size)<<endl;
+  return 0;
+}
+*/
+
+//OPTIMISED SOLUTION
+
+#include<bits/stdc++.h>
+using namespace std;
+int majority(int arr[],int size){
+   int res=0,count=1;
+   for(int i=1;i<size;i++){
+      if(arr[res]==arr[i]){
+         count++;
+      }
+      else{
+         count--;
+      }
+      if(count==0){
+         res=i;
+         count=1;
+      }
+   }
+   count=0;
+   for(int i=0;i<size;i++){
+      if(arr[res]==arr[i]){
+         count++;
+      }
+   }
+   if(count<=size/2){
+      return -1;
+   }
+   return res;
+}
+int main(){
+   int arr[]={8,3,4,8,8,3};
    int size=sizeof(arr)/sizeof(arr[0]);
    cout<<majority(arr,size)<<endl;
   return 0;
