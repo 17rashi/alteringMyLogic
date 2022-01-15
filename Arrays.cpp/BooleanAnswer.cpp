4398ab96307ee7e9baaef420eb632ccf10ad4453
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-01-15 10:04:07 +0530

    updated boolean answer

--- Arrays.cpp/BooleanAnswer.cpp
+++ Arrays.cpp/BooleanAnswer.cpp
@@ -1,33 +1,31 @@
 //https://mycode.prepbytes.com/problems/arrays/NOMERCY
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-   int T;
-   cin>>T;
-   while(T--){
-      int n,m,count=0,res=0,max=0;
-      cin>>n>>m;
-      int arr[10000][10000];
-      for(int i=0;i<n;i++){
-        count=0;
-         for(int j=0;j<m;j++){
-            cin>>arr[i][j];
-            if(arr[i][j]==1){
-               count++;
-            }
-         }
-         if(count>0 && count>res){
-            res=count;
-            max=i;
-         }
+
+int main()
+{
+  //write your code here
+  int T;
+  cin>>T;
+  while(T--){
+    int n,m,count=0,max=-1,res=0;
+    cin>>n>>m;
+    int arr[10000][10000];
+    for(int i=0;i<n;i++){
+      count=0;
+      for(int j=0;j<m;j++){
+        cin>>arr[i][j];
+        if(arr[i][j]==1){
+          count++;
+        }
       }
-      if(res>0){
-         cout<<max<<endl;
+      if(count>0 && count>res){
+        res=count;
+        max=i;
       }
-      else{
-         cout<<-1<<endl;
-      }
-   }
+    }
+      cout<<max<<endl;
+  }
   return 0;
 }
