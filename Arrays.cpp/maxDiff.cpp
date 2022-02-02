7cdadf51c6e78963230aba9ea01daad7b2b661a6
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-02-02 23:39:45 +0530

    max diff 1 more approach

--- Arrays.cpp/maxDiff.cpp
+++ Arrays.cpp/maxDiff.cpp
@@ -39,3 +39,35 @@ int main(){
    cout<<maxDifference(arr,size)<<endl;
   return 0;
 }
+
+
+//Optimized approach
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int maxDifference(int arr[],int n){
+//    int min_element=arr[0],max_diff=0,diff=0;
+//    int max_element=0;
+//    int p=0,q=0;
+//    for(int i=1;i<n;i++){
+//       if(arr[i]<min_element){
+//          p=i;
+//          min_element=arr[i];
+//       }
+//       if(arr[i]>max_element){
+//          q=i;
+//          max_element=arr[i];
+//       }
+//       if(p<q){
+//          diff=max_element-min_element;
+//       }
+//       max_diff=max(max_diff,diff);
+//    }
+//    return max_diff;
+// }
+// int main(){
+//    int arr[]={80, 2, 6, 3, 100};
+//    int size=sizeof(arr)/sizeof(arr[0]);
+//    cout<<maxDifference(arr,size)<<endl;
+//   return 0;
+// }
