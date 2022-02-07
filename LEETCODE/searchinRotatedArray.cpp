1b278eb829b3008160f9095a04361d330f2ffecb
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-02-07 23:22:46 +0530

    search in rotated array leetcode problem

---
+++ LEETCODE/searchinRotatedArray.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       for(int i=0;i<nums.size();i++){
+            if(nums[i]!=target){
+
+            }
+           else{
+                return true;
+           }
+       } 
+        return false;
+    }
+};
