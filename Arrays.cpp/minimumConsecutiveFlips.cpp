d5d9578b45091ad5a938d563ae15bfc0aa59f6e8
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-02-11 23:26:16 +0530

    minimum consecutive flips

--- Arrays.cpp/minimumConsecutiveFlips.cpp
+++ Arrays.cpp/minimumConsecutiveFlips.cpp
@@ -1,18 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 void printGroups(bool arr[],int n){
+   int first=arr[0];
    for(int i=1;i<n;i++){
       if(arr[i]!=arr[i-1]){
-         if(arr[i]!=arr[0]){
-            cout<<"From "<<i<<" to ";
+         if(arr[i]!=first){
+            cout<<"From "<<i+1<<" to ";
          }
          else{
-            cout<<(i-1)<<endl;
+            cout<<(i)<<endl;
          }
       }
    }
    if(arr[n-1]!=arr[0]){
-      cout<<(n-1)<<endl;
+      cout<<(n)<<endl;
    }
 }
 int main(){
@@ -25,3 +26,9 @@ int main(){
    printGroups(arr,n);
   return 0;
 }
+
+//output
+/* 
+arr[]={1,1,0,0,0,1,1,0,0,1};
+output = 2
+*/
