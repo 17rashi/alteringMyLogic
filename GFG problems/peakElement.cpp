bf5065cec0eb7cc218413c0c37fab7703b5e0b6e
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-02-19 15:13:43 +0530

    peak element - Leetcode Problem

--- GFG problems/peakElement.cpp
+++ GFG problems/peakElement.cpp
@@ -29,3 +29,26 @@ int main() {
 
 	return 0;
 }  
+
+//https://leetcode.com/problems/find-peak-element/
+
+/*
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+               return i;
+            }
+        }
+        return 0;
+    }
+};
+*/
