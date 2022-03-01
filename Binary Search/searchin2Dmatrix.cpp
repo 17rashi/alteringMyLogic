2aba4cf5ab33b432163254df3dc2080b95efda1b
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-03-01 22:06:44 +0530

    search in 2D matrix - LEETCODE Problem

---
+++ Binary Search/searchin2Dmatrix.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/search-a-2d-matrix-ii/submissions/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=0;
+        int c=matrix[0].size()-1;
+        while(r<matrix.size() && c>=0){
+            // cout<<matrix[r][c]<<" ";
+            if(matrix[r][c]==target){
+                return true;
+            }
+            else if(matrix[r][c]>target){
+                c--;
+            }
+            else{
+                r++;
+            }
+        }
+        return false;
+    }
+};
