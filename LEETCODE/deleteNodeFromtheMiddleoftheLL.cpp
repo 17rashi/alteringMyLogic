397c7c068c2d7fe412de2f0461b12bee2046b8dc
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-03-29 22:54:08 +0530

    Leetcode medium level question

---
+++ LEETCODE/deleteNodeFromtheMiddleoftheLL.cpp
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(ListNode* head){
+        int count=0;
+        while(head!=NULL){
+            count++;
+            head = head -> next;
+        }
+        return count/2;
+    }
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return NULL;
+        }
+        ListNode* temp = head;
+        int m=countNodes(head);
+        int counter=1;
+        while(head-> next !=NULL){
+            if(counter<m){
+                head = head -> next; 
+                counter++;
+            }
+            else{
+                head->next = head -> next -> next;
+                break;
+            }
+        }
+        
+        return temp;
+    }
+};
