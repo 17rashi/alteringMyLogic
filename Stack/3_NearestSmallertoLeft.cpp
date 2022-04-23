f2dbfa0d3a059e9b7c26bec9a6a0d9e991238747
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2022-04-23 23:48:28 +0530

    nearest smaller element to left and right

---
+++ Stack/3_NearestSmallertoLeft.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> nextSmaller(vector<int> arr){
+   stack<int> S;
+   vector<int> ans;
+   S.push(-1);
+   for(int i=0;i<arr.size();i++){
+      if(S.top()==-1){
+         ans.push_back(S.top());
+      }
+      else{
+         while(S.top()!=-1 && S.top()>arr[i]){
+            S.pop();
+         }
+         ans.push_back(S.top());
+      }
+      S.push(arr[i]);
+   }
+   return ans;
+}
+int main(){
+   int n;
+   cin>>n;
+   vector<int> a;
+   for(int i=0;i<n;i++){
+      int e;
+      cin>>e;
+      a.push_back(e);
+   }
+   vector<int> ans=nextSmaller(a);
+   for(int i=0;i<n;i++){
+      cout<<ans[i]<<" ";
+   }
+  return 0;
+}
